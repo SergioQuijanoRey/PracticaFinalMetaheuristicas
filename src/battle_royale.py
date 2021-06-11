9b3e2520ca999b2a6da454e4e3849fa27f93b6d5
Sergio Quijano Rey <sergiquijano@gmail.com>
2021-06-11 16:06:29 +0200

    feat: escribo una estructura basica para la metaheuristica que quiero desarrollar

--- src/battle_royale.py
+++ src/battle_royale.py
@@ -1,10 +1,80 @@
+import numpy as np
+
 class BattleRoyale:
     """Clase que representa la metaheuristica que desarrollamos"""
 
+    def __init__(self, dimension, ev_per_dimension):
+        self.dimension = dimension
+        self.ev_per_dimension = ev_per_dimension
+
+    def search(self):
+        best_solution = Player.random_player(self.dimension)
+
+        evals = 0
+        while evals < self.dimension * self.ev_per_dimension:
+            print("..", end=" - ")
+            evals += 1000
+        print("")
+
+        return best_solution
+
+class Popuation:
+    """Representa una poblacion de jugadores (soluciones)"""
     def __init__(self):
         pass
 
-class Solution:
-    """Clase que representa una solucion al problema considerado"""
-    def __init__(self):
+
+# Constantes asociadas a las funciones con las que trabajamos
+#===============================================================================
+lower_range = -100
+upper_range = 100
+
+class Player:
+    """Clase que representa un jugador (solucion) al problema considerado"""
+
+    def __init__(self, dimension, genes):
+        self.dimension = dimension
+        self.genes = genes
+
+        if self.is_valid() == False:
+            raise Exception("Player.BadInit: new solution is not valid, bad initializing")
+
+    def random_player(dimension):
+        """Genera un jugador con genes aleatorios"""
+        genes = np.random.randint(lower_range, upper_range, dimension)
+        rand_player = Player(dimension, genes)
+        return rand_player
+
+    def is_valid(self):
+        """
+        Comprueba que el jugador represente una solucion valida
+        Returns:
+        ========
+        true: si el jugador es valido
+        false: si el jugador no es valido
+        """
+
+        # El tamaño del vector solucion debe coincidir con el tamaño especificado
+        if len(self.genes) != self.dimension:
+            return False
+
+        # Los genes estan en el rango permitido para las funciones con las que trabajamos
+        for gene in self.genes:
+            if gene < lower_range or gene > upper_range:
+                return False
+
+        # Todas las comprobaciones han sido superadas
+        return True
+
+    def to_list(self):
+        """
+        Convierte el jugador en una lista de python, para poder ser usada en funciones de la api que
+        nos dan los profesores de practicas.
+        """
+        return self.genes.tolist()
+
+    def fitness(self):
+        """Devuelve el fitness asociado a este jugador"""
         pass
+
+
--- src/main.py
+++ src/main.py
@@ -1,12 +1,13 @@
 import numpy as np
 import cec17
+from battle_royale import BattleRoyale, Player
 
 if __name__ == "__main__":
     print("==> Inicializamos la semilla aleatoria")
     np.random.seed(123456789)
 
     # Evaluaciones del fitness que tenemos por cada dimension del problema
-    ev_per_dimension = 10000
+    ev_per_dimension = 10_000
 
     # Dimension del problema con el que trabajamos
     dimension = 10
@@ -17,17 +18,19 @@ if __name__ == "__main__":
         evals = 0
 
         # Fijamos la funcion a utilizar en esta iteracion
+        # Con ello, el resto de llamadas cec17 usan los datos de la funcion especificada (en particular,
+        # el fitness)
         cec17.init("pruebas", function_id, dimension);
 
         # Para mostrar los mensajes por pantalla
         cec17.print_output();
 
-        # Iteramos mientras no hayamos consumido las evaluaciones
-        while evals < dimension * ev_per_dimension:
-            print("..", end=" - ")
-            evals += 1000
-        print("")
+        # Inicalizamos nuestra metaheuristica
+        mh = BattleRoyale(dimension = dimension, ev_per_dimension = ev_per_dimension)
+
+        # Tomamos la solucion a partir de la busqueda
+        best_player = mh.search()
 
     # Mostramos el error alcanzando
-    err = cec17.fitness(np.zeros(dimension), dimension)
+    err = cec17.fitness(best_player.to_list(), dimension)
     print(f"Error alcanzado: {err}")
