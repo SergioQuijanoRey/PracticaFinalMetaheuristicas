9229baccb1acaf259731d33dc0d8db961ca128fe
Sergio Quijano Rey <sergiquijano@gmail.com>
2021-06-19 19:28:23 +0200

    fix: los resultados de standar y memetico se guardaban en la misma carpeta

--- launch_experiments.sh
+++ launch_experiments.sh
@@ -20,10 +20,19 @@ echo "Lanzamos los experimentos para Standar - dimension 10 "
 pipenv run python3 main.py 10 standar
 echo "Lanzamos los experimentos para Standar - dimension 30"
 pipenv run python3 main.py 30 standar
+
+# Movemos la carpeta a otra para que los resultados de memeticos no sobreescriban los resultados
+mv results_battle_royale results_battle_royale_standar
+mkdir results_battle_royale
+
+# Lanzamos ahora la version memetica
 echo "Lanzamos los experimentos para Memetic - dimension 10 "
 pipenv run python3 main.py 10 memetic
 echo "Lanzamos los experimentos para Memetic - dimension 30"
 pipenv run python3 main.py 30 memetic
+
+# Movemos la carpeta para que sea mas claro lo que representa
+mv results_battle_royale results_battle_royale_memetic
 
 # Volvemos al directorio original
 cd -
